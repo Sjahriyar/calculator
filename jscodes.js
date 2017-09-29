85e448ed70bb268bb109bba8d6767769c4a15b61
Sjahriyar <30406716+Sjahriyar@users.noreply.github.com>
2017-09-29 14:02:46 +0200

    Little bit defence added

--- jscodes.js
+++ jscodes.js
@@ -1,39 +1,48 @@
 
 $(document).ready(function(){
 
-
-
-
-	var sum = 0;
+//Required variables.
 	var operator = "";
 	var total = $("#input");
 	var nums = $(".numbers div");
 	total.text('0');
 
-
+//Error handler function, in case input number is greater
+//than 9 or 15.
 	var testLength = function()
 	{
 		if (total.text().length > 9)
 		{
 			return false;
-			// number.substr(number.length-9,9);
+			// substr(total.text().length-9,9);
 		}
-		return true;
+
 		if (total.text().length > 15)
 		{
-			total.text();// = "";
-			total.text( "Wow! HUGE!" );
-		}
-	}
+			// total.text();// = "";
+			substr(total.text().length-9,9);
 
+		}
+		return true;
+	}
+	//Error handler for ppl who r trying to break the app.
 
 	nums.click(function(){
-		if( total.text() === '0')
+		if( total.text() === '0'){
 			total.text('');
+		}
 		if( testLength() )
 			total.text( total.text() + $(this).text());
 
+			if (total.text() === '0/0'){
+				total.text('Ted? is that you ?')
+			}
+
+			// if (total.text() === '1/0' || '2/0'){
+			// 	total.text('Ted? is that you ?')
+			// }
 	})
+
 
 	$(".operators div").click(function(){
 		total.text( total.text() + $(this).text());
@@ -44,8 +53,14 @@ $(document).ready(function(){
 
 
 $("#result").click(function(){
-	total.text(eval(total.text()));
 
+	if(testLength()){
+	total.text(eval(total.text()));
+	if (total.text() == Number.POSITIVE_INFINITY )
+	{
+total.text('Ted? is that you ?')
+	}
+}
  // switch (operator){
  // 	case "+":
  // 		operate = parseFloat(number1) + parseFloat(number2);
@@ -63,8 +78,8 @@ $("#result").click(function(){
  // }
 	// total.text(operate);
 })
+total.text('0');
 $("#clear").click(function(){
-
-	total.text(sum);
+	total.text(0);
 })
 }); //Everything ends here!
